f0c349e41243520f736feac11658846fcc084316
Jose Rios <joserios0531@gmail.com>
2026-06-17 17:44:17 -0700

    Css color previews are now working

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -2,6 +2,20 @@ return {
     "fxn/vim-monochrome",
     config = function()
         vim.cmd.colorscheme("monochrome")
-        vim.api.nvim_set_hl(0, "Comment", { fg = "#FFD700" })
+
+        -- All this extra gibberish is to not have the nvim-highlight-colors overwritten
+        local function set_highlights()
+            vim.api.nvim_set_hl(0, "Comment", { fg = "#FFD700" })
+            vim.api.nvim_set_hl(0, "Pmenu", { bg = "#1e222a", fg = "#abb2bf" })
+            vim.api.nvim_set_hl(0, "PmenuSel", { fg = "#FFD700" })
+            require('nvim-highlight-colors').setup({})
+        end
+
+        set_highlights()
+
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            pattern = "*",
+            callback = set_highlights,
+        })
     end
 }
