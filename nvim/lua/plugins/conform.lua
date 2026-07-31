384891cdca413b87ffb9a80b1225badd46c22b61
josedrios <joserios0531@gmail.com>
2026-07-31 10:13:20 -0700

    pushing possible additions i missed

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
             javascriptreact = { "prettier" },
             typescript = { "prettier" },
             typescriptreact = { "prettier" },
+            json = { "prettier" },
             css = { "prettier" },
         },
         format_on_save = { timeout_ms = 500, lsp_fallback = true },
