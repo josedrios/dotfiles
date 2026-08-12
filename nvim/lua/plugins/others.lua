a1b6f4fa14bc474eb8edde873c7ccf248e13cc1f
josedrios <joserios0531@gmail.com>
2026-08-12 11:15:25 -0700

    add folke todo comments plugin

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -14,5 +14,14 @@ return {
         config = function()
             require("nvim-ts-autotag").setup()
         end
+    },
+    { -- Highlight/color todo comments
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
     }
 }
